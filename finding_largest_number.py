56a41104598e98a44bdfbb5355f7a357a85b776c
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-11 16:50:27 +0800

    first number

---
+++ finding_largest_number.py
@@ -0,0 +1,12 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
+
+# pseudocode
+
+# ask the user to input 3 numbers
+first_number = input("Enter the first number: ")
+print (first_number)
+# second number
+# third number
+
+# find the largest among the 3 numbers using the if-else statement
+# print the output
