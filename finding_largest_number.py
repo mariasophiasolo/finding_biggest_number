d48aa48588bc02c5faaa586393d6370e89c1a622
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-11 17:29:40 +0800

    if-else statement

--- finding_largest_number.py
+++ finding_largest_number.py
@@ -2,13 +2,20 @@
 
 # pseudocode
 
-# ask the user to input 3 numbers
-first_number = input("Enter the first number: ")
-second_number = input("Enter the second number: ")
-third_number = input("Enter the third number: ")
 
-# second number
-# third number
+
+# ask the user to input 3 numbers
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+third_number = int(input("Enter the third number: "))
 
 # find the largest among the 3 numbers using the if-else statement
+if first_number >= second_number and first_number >= third_number:
+    largest_number = first_number
+elif second_number >= third_number:
+    largest_number = second_number
+else:
+    largest_number = third_number
+
 # print the output
+print("The largest nnumber: ", largest_number)
