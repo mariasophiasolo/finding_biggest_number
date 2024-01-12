4ac472eff80cc04b0755ab5ba6018f7e43fdc80a
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-12 11:37:07 +0800

    fonts

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -9,6 +9,9 @@ window.minsize(width=355, height=500)
 window.configure(background="black")
 window.resizable(False,False)
 
+number_label = tk.Label(window, text="P L E A S E  E N T E R  T H R E E  N U M B E R S", bg="black", fg="khaki1", font=("Glacial Indifference", 10))
+number_label.place(x=25, y=360)
+\
 def find_and_print_the_biggest_number():
     # ask the user to input 3 numbers
     first_number = int(number1.get())
@@ -22,26 +25,25 @@ def find_and_print_the_biggest_number():
         biggest_number = second_number
     else:
         biggest_number = third_number
-    
-    # Display the result
-    result_label.config(text=f"The biggest number is: {biggest_number}")
+    result_label.config (text=f"The Biggest Number Is:\n {biggest_number}")
 
-poster = tk.PhotoImage (file="biggest.png")
+poster = tk.PhotoImage (file="math.png")
 poster_label = tk.Label(image=poster,borderwidth=0)
 poster_label.place(x=0, y=0)
+
 # print the output
 number1 = tk.Entry(window, width=8)
 number2 = tk.Entry(window, width=8)
 number3 = tk.Entry(window, width=8)
 
-number1.place(x=45, y=50)
-number2.place(x=150, y=50)
-number3.place(x=256,y=50)
+number1.place(x=45, y=385)
+number2.place(x=150, y=385)
+number3.place(x=256,y=385)
 
 find_button = tk.Button (window, text = "Find the Biggest Number", command=find_and_print_the_biggest_number)
-find_button.place (x=30,y=140)
+find_button.place (x=105,y=460)
 
-result_label = tk.Label(window, text="")
-result_label.place(x=30, y=170)
+result_label = tk.Label(window, text="The Biggest Number:", font=("Glacial Indifference", 10, "bold"), fg="khaki1", bg="black",)
+result_label.place(x=104, y=420)
 
 window.mainloop()
